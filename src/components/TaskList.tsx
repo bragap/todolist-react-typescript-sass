612a499a841bcc5cf5771e71346eb659255ac604
Pedro Henrique <130567453+pedrohbragac@users.noreply.github.com>
2023-11-01 15:09:00 -0300

    versão final

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -11,23 +11,29 @@ type Props = {
 }
 
 const TaskList = ({taskList, handleDelete, handleEdit}: Props) => {
+ 
   return (
     <>
       {taskList.length > 0 ? (
-        taskList.map((task) => (
-          <div key={task.id} className={styles.task}>
-            <div className={styles.details}>
-              <h4>{task.title}</h4>
-              <p>Dificuldade: {task.difficulty}</p>
-            </div>
-            <div className={styles.actions}>
-              <i className="bi bi-pencil" onClick={() => {
-                handleEdit(task)
-              }}></i>
-              <i className="bi bi-trash" onClick={() => {handleDelete(task.id)}}></i>
-            </div>
-          </div>
-        ))
+        taskList.map((task) => {
+          if (task.title && task.difficulty) {
+            return (
+              <div key={task.id} className={styles.task}>
+                <div className={styles.details}>
+                  <h4>{task.title}</h4>
+                  <p>Dificuldade: {task.difficulty}</p>
+                </div>
+                <div className={styles.actions}>
+                  <i className="bi bi-pencil" onClick={() => {
+                    handleEdit(task)
+                  }}></i>
+                  <i className="bi bi-trash" onClick={() => {handleDelete(task.id)}}></i>
+                </div>
+              </div>
+            );
+          }
+          return null;
+        })
       ) : (
         <p>Não há tarefas cadastradas!</p>
       )}
